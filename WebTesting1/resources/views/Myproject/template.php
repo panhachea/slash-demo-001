edae6a5fda4f52eb59846fda7b188e83aaa0e33f
panhachea <cheapanha04@gmail.com>
2017-07-13 08:33:04 +0700

    my WebTemplate

---
+++ WebTesting1/resources/views/Myproject/template.php
@@ -0,0 +1,211 @@
+<!doctype>
+<html>
+<head >
+    <title>
+
+    </title>
+    <link href="css/template.css" rel="stylesheet" type="text/css" media="screen">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Condensed">
+</head>
+<body>
+
+<div class="container">
+    <div class="header">
+
+    <img src="img/pork.jpg"  />
+
+
+    <div class="logo">
+        <span style="background:yellow;width:18px;" >Y</span>
+        <span  style="background: blue;width:18px;">S</span>
+        <span  style="background: green;width:18px;">L</span><br/>
+        <p style="font-size:13px;margin-left:-2px;color:gainsboro">YSL company</>
+    </div>
+
+     <div class="navi">
+
+         <ul>
+             <li>Contact Us</li>
+             <li>Quote Request</li>
+             <li>Product & Service</li>
+             <li>About</li>
+
+         </ul>
+     </div>
+
+      <div class="sub_bg_img">
+      <p class="sub_text">YELLOW SEA LOGISTICS offers the suports for the goosds transportation both locally and globally as ,Rail Freight,
+          Intermodal,Air Freight,Ocean Freigth,Custom Broker, and Warehousing/Distribution.</p>
+          <br>
+          <button class="btn">Quote Request</button>
+      </div>
+
+        <div class="letter">
+            <h4 class="about">About<span style="color:yellow;"> Yellowsea Logistic</span></h4>
+            <p>Yellow Sea Logistics offers the supports for the goods transportation both locally and globallly such as Rail
+                Freight. Intermodal, Air Freight, Custom Broker, and WarehousingDistrubute.</p>
+            <p>Bason on our strong assets and facilities, we have beenmotivated to start out first
+                service providing in 2016, and then with many more services from the start to the end.
+                We would like our customers to rest assured and let us take care of the rest for you.We are new
+                in term of company,but we havea talented and
+                experience learn here to ensure a high quality servicebeing delivered and satisfiedour cutomers.</p>
+             <p>We are doing our best as a respondiblr budinrdd partner and a customer tocused service provider
+                 to bring value and solution to your business developmentas well as ours.</p>
+        </div>
+        <div class="last_header_img">
+            <img src="img/kokaner.jpg" class="last_pic">
+
+
+            <div class="logos">
+                <span style="background:yellow;width:18px;" >Y</span>
+                <span  style="background:lightblue;width:18px;">S</span>
+                <span  style="background: green;width:18px;">L</span><br/>
+                <p style="font-size:13px;margin-left:-2px;color:black">YSL company</>
+            </div>
+
+
+        </div>
+
+    </div>
+
+
+
+    <div class="Body">
+        <h4 class="title">Product & Service</h4>
+        <hr>
+
+        <div class="first_title">
+
+        <div class="text_left">
+            <h3>Intermodal Service</h3>
+            <p>our own product is to provide you the stable but qualitied delivery service.We have a
+                station where you can essily drop your cargo and have everything to us.On top of that are have
+                the intermodal service to support you an all in delivery to yor warehouse directly.</p>
+            <p>
+                We have a strong product as we have a warehouse combined with locak intermodal service.
+                It will sarve youome,oop and security.
+                Your cargos will dilivered at the destination all at the same time.</p>
+
+        </div>
+        <div class="pic_right">
+            <img src="img/cat.jpg" style="height:170px;">
+
+        </div>
+
+        </div>
+
+        <div class="sec_line"><br>
+            <hr><br>
+            <div class="pic_left">
+                <img src="img/cat.jpg" style="height: 170px;">
+
+            </div>
+
+
+            <div class="text_right">
+            <h3> Freight Forwarding</h3>
+                <p>our own product is to provide you the stable but qualitied
+                    delivery service.We have a station where you can essily drop
+                    your cargo and have everything to us.On top of that are have
+                    the intermodal service to support you an all in delivery to yor warehouse directly.
+                </p>
+                <p>
+                    We have a strong product as we have a warehouse combined with locak intermodal service.It will sarve youome,
+                    oop and security. Your cargos will dilivered at the destination all at the same time.</p>
+            </div>
+        </div>
+
+        <div class="thr_line">
+            <br>
+            <hr>
+            <div class="Text_lefts">
+                <h3>Custom House Brokerage</h3>
+                <p>our own product is to provide you the stable but qualitied delivery
+                    service.We have a station where you can essily drop your cargo and have everything to us.On top of that are have
+                    the intermodal service to support you an all in delivery to yor warehouse directly.</p>
+                <p>We have a strong product as we have a warehouse combined with locak intermodal service.It will
+                    sarve youome,oop and security.
+                    Your cargos will dilivered at the destination all at the same time.</p>
+
+            </div>
+
+            <div class="Pic_rights">
+                <img src="img/pork.jpg" style="height:170px;">
+
+            </div>
+
+        </div>
+
+        <div class="last_line">
+            <br>
+            <hr>
+            <div class="Pic_Left">
+                <img src="img/pork.jpg" style="height: 170px">
+
+            </div>
+            <div class="Text_Right">
+                <h3>Warehousing And Distribution</h3>
+                <p>our own product is to provide you the stable but
+                    qualitied delivery service.We have a station where
+                    you can essily drop your cargo and have everything to
+                    us.On top of that are have the intermodal service to support
+                    you an all in delivery to yor warehouse directly.
+                </p>
+                <p>We have a strong product as we have a warehouse combined with locak intermodal service
+                    .It will sarve youome,oop and security. Your cargos will dilivered at the destination all at the same time.</p>
+
+            </div>
+        </div>
+        <br>
+        <hr style="margin-left:43%; ">
+    </div>
+
+
+    <div class="footer">
+
+    <div class="Last_bg">
+        <img src="img/pork.jpg" style="height:300px;">
+        <div class="mirror_bg">
+            <p class="bg_text">YELLOW SEA LOGISTICS offers the suports for the goosds transportation both locally and globally as
+                ,Rail Freight,Intermodal,Air Freight,Ocean Freigth,Custom Broker, and Warehousing/Distribution.</p>
+            <button class="btn_last">Quote Request</button>
+
+        </div>
+        <h3 class="follow">Follow US</h3>
+        <div class="Socailimg">
+            <img src="img/facebook.png"  style="height:40px; width:40px;margin-left: 540px;"/>
+            <img src="img/twitter.png"  style="height: 40px;width:40px;margin-left: 15px; " >
+            <img src="img/instragram.png" style="height: 40px; width:40px;margin-left: 15px;">
+            <img src="img/google.png" style="height: 40px; width:40px;margin-left: 15px;" >
+            <img src="img/youtube.jpg" style="height: 40px; width:40px;margin-left: 15px;">
+
+
+
+        </div>
+    </div>
+    <div class="end">
+        <ul>
+            <li>Contact Us</li>
+            <li>Quote Request</li>
+            <li>Product & Service</li>
+            <li>About</li>
+        </ul>
+          </div>
+
+        <p class="last_copy">@2017 Yellow Sea Logistic .All right reserved Designed by Flexitech</p>
+
+    </div>
+
+    </div>
+
+
+
+
+
+</div>
+
+
+</body>
+</html>
---
+++ WebTesting1/resources/views/home.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Getting Srated 002</title>
+    <link rel="stylesheet" type="text/css" href="css/home.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto+Condensed">
+</head>
+
+<body>
+<div class="Container_header">
+    <img src="img/pork.jpg" class="BackgroundImg">
+    <div class="NavigationBar font-effect-fire-animation">
+        <div class="logo">
+            <span style="background-color:#DFD10F">Y</span>
+            <span style="background-color:#42A5ED">S</span>
+            <span style="background-color:#0E8F14">L</span>
+            <p>YSL company</p>
+        </div>
+        <div class="NavigationList">
+            <ul>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Product &amp; Services</a></li>
+                <li><a href="#"> Quote Request</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="intro_INHeader">
+        <p>
+            YELLOW SEA LOGISTICS offers the suports for the goosds transportation both locally and globally as ,Rail Freight,Intermodal,Air Freight,Ocean Freigth,Custom Broker, and Warehousing/Distribution.
+        </p>
+    </div>
+    <button class="Button_head">QUOTE REQUEST</button>
+
+</div><!-- endl Header-->
+<div class="About_Container">
+    <div class="About_text">
+        <h2>About</h2>
+        <h2 class="yellow">Yellowsea Logistic</h2>
+        <p>
+            Yellow Sea Logistics offers the supports for the goods transportation both locally and globallly such as Rail Freight. Intermodal, Air Freight, Custom Broker, and WarehousingDistrubute.<br><br>
+            Bason on our strong assets and facilities, we have beenmotivated to start out first service providing in 2016, and then with many more services from the start to the end. We would like our customers to rest assured and let us take care of the rest for you.We are new in term of company,but we havea talented and experience learn here to ensure a high quality servicebeing delivered and satisfiedour cutomers.<br><br>
+            We are doing our best as a respondiblr budinrdd partner and a customer tocused service provider to bring value and solution to your business developmentas well as ours.
+        </p>
+    </div>
+    <div class=" About_ContainImg">
+        <img src="img/kokaner.jpg">
+        <div class="NavigationBar">
+            <div class="logo">
+                <span style="background-color:#DFD10F">Y</span>
+                <span style="background-color:#42A5ED">S</span>
+                <span style="background-color:#0E8F14">L</span>
+                <p>YSL company</p>
+            </div>
+
+        </div>
+    </div>
+</div>
+<div class="Product_container">
+    <div class="Product_title">
+        <h3>PRODUCT &amp; SERVICE</h3>
+    </div>
+
+    <div class="Produc_ContainImgAndDes">
+        <div class="Product_Des">
+            <h4>Intermodal Service</h4>
+            <p>
+                our own product is to provide you the stable but qualitied delivery service.We have a station where you can essily drop your cargo and have everything to us.On top of that are have the intermodal service to support you an all in delivery to yor warehouse directly.<br><br>
+                We have a strong product as we have a warehouse combined with locak intermodal service.It will sarve youome,oop and security. Your cargos will dilivered at the destination all at the same time.
+            </p>
+        </div>
+        <div class="Product_img">
+            <img src="img/cat.jpg"/>
+        </div>
+        <hr align="center" color="#DFD10F" size="2px">
+    </div>
+    <!-- end Intermodal Service-->
+
+    <div class="Produc_ContainImgAndDes">
+        <div class="Product_img" style="float:left">
+            <img src="img/cat.jpg" style="margin-left:0px;"/>
+        </div>
+        <div class="Product_Des" style="float:right">
+            <h4>Freight Forwarding</h4>
+            <p>
+                our own product is to provide you the stable but qualitied delivery service.We have a station where you can essily drop your cargo and have everything to us.On top of that are have the intermodal service to support you an all in delivery to yor warehouse directly.<br><br>
+                We have a strong product as we have a warehouse combined with locak intermodal service.It will sarve youome,oop and security. Your cargos will dilivered at the destination all at the same time.
+            </p>
+        </div>
+        <hr align="center" color="#DFD10F" size="2px">
+    </div>
+    <!-- endl Freight Forwarding -->
+    <div class="Produc_ContainImgAndDes">
+        <div class="Product_Des">
+            <h4>Custom House Brokerage</h4>
+            <p>
+                our own product is to provide you the stable but qualitied delivery service.We have a station where you can essily drop your cargo and have everything to us.On top of that are have the intermodal service to support you an all in delivery to yor warehouse directly.<br><br>
+                We have a strong product as we have a warehouse combined with locak intermodal service.It will sarve youome,oop and security. Your cargos will dilivered at the destination all at the same time.
+            </p>
+        </div>
+        <div class="Product_img">
+            <img src="img/cat.jpg"/>
+        </div>
+        <hr align="center" color="#DFD10F" size="2px" >
+    </div>
+    <!-- end  Customer House Brikerage-->
+
+    <div class="Produc_ContainImgAndDes">
+        <div class="Product_img" style="float:left">
+            <img src="img/cat.jpg" style="margin-left:0px;"/>
+        </div>
+        <div class="Product_Des" style="float:right">
+            <h4>Warehousing And Distribution</h4>
+            <p>
+                our own product is to provide you the stable but qualitied delivery service.We have a station where you can essily drop your cargo and have everything to us.On top of that are have the intermodal service to support you an all in delivery to yor warehouse directly.<br><br>
+                We have a strong product as we have a warehouse combined with locak intermodal service.It will sarve youome,oop and security. Your cargos will dilivered at the destination all at the same time.
+            </p>
+        </div>
+        <hr align="center" color="#DFD10F" size="2px" >
+    </div>
+
+</div><!-- end product view-->
+
+<div class="bottoom_img">
+    <img src="img/kokaner.jpg" alt="warehouse"/>
+    <div class="intro_INHeader MoreOnIntro">
+        <p>
+            YELLOW SEA LOGISTICS offers the suports for the goosds transportation both locally and globally as ,Rail Freight,Intermodal,Air Freight,Ocean Freigth,Custom Broker, and Warehousing/Distribution.
+        </p>
+    </div>
+    <button class="Button_head MoreOnBtn">QUOTE REQUEST</button>
+</div>
+<!-- end image at bottom-->
+<div class="FollowUs_contain">
+    <div class="Product_title">
+        <h3 style="border:none"> FOLLOW US</h3>
+    </div>
+    <div class="contain_socialMedia">
+        <!-- Add font awesome icons -->
+        <a href="#" class="fa fa-facebook"></a>
+        <a href="#" class="fa fa-twitter"></a>
+        <a href="#" class="fa fa-instagram"></a>
+        <a href="#" class="fa fa-google"></a>
+        <a href="#" class="fa fa-youtube"></a>
+    </div>
+</div>
+<div class="FollowUs_contain atBottom">
+    <ul>
+        <li><a href="#" >About Us</a></li>
+        <li><a href="#" >Product &amp; Services</a></li>
+        <li><a href="#" > Quote Request</a></li>
+        <li><a href="#" >Contact Us</a></li>
+    </ul>
+    <p>@2017 Yellow Sea  Logistic .All right reserved Designed by Flexitech</p>
+
+</div>
+
+</body>
+</html>
